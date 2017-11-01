70be6b9f35cd71928edaa4719b5a46dab5b069ee
pelenovski <31411489+pelenovski@users.noreply.github.com>
2017-11-01 10:29:28 -0500

    Color Lab

---
+++ color-lab-pelenovski-master/ColorLabs17/ColorLab.java
@@ -0,0 +1,20 @@
+//Pane Elenovski
+//11/1/2017
+//ColorLab
+//Do stuff to a picture
+
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
+public class ColorLab
+{
+    public static void main(String[] args)
+    {
+        Picture pic = new Picture("images\\thepic.jpg");
+        pic.explore();
+        Picture pic1 = new Picture("images\\thepic.jpg");
+        
+        
+    }
+}
